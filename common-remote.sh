0155338df14e26edd984630734d80352deaf3dfd
Marcel Keller <mks.keller@gmail.com>
2019-09-16 13:12:03 +0200

    Don't transfer unnecessary files.

--- common-remote.sh
+++ common-remote.sh
@@ -10,7 +10,7 @@ done
 . `dirname $0`/common-setup.sh
 
 for host in $host0 $host1 $host2; do
-    rsync -av Player-Data Programs $host:`pwd`
+    rsync -avR Player-Data/*.{pem,key,0} Programs $host:`pwd`
 done
 
 i=0
